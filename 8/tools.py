8334ea1c9d6f119aee72b7df64c44de5c33a0340
ksasakipz <ksasaki@students.pitzer.edu>
2021-12-09 10:08:00 -0800

    day 8 part 2 checkpoint (doesn"t work)

--- 8/tools.py
+++ 8/tools.py
@@ -8,8 +8,44 @@ def parse_line(line):
     first_half = temp1[0].strip().split(" ")
     second_half= temp1[1].strip().split(" ")
 
-    retval = [first_half, second_half]
+    retval = first_half + second_half
+    retval = decode(retval)
     return retval
+
+def decode(numbers):
+    finished = False
+    fivers = False
+    sixers = False
+    d = {}
+    for i in range(10):
+        d[i] = 0
+    for i in range(len(numbers)):
+        if len(numbers[i]) == 2:
+            d[1] = ''.join(sorted(numbers[i]))
+        elif len(numbers[i]) == 3:
+            d[7] = ''.join(sorted(numbers[i]))
+        elif len(numbers[i]) == 4:
+            d[4] = ''.join(sorted(numbers[i]))
+        elif len(numbers[i]) == 7:
+            d[8] = ''.join(sorted(numbers[i]))
+        elif len(numbers[i]) == 5:
+            fivers = True
+        elif len(numbers[i]) == 6:
+            sixers = True
+        else:
+            raise Exception("ack")
+    
+    if sixers:
+        pass
+    if fivers:
+        pass
+    
+    while not finished:
+        break
+        pass
+        
+    retval = 0
+    return numbers
 
 def load_data(fname):
     in_file = open(fname, 'r')
@@ -21,7 +57,7 @@ def load_data(fname):
             break
         x = parse_line(line)
 
-        for i in x[1]:
+        for i in x[10:]:
             if len(i) == 2 or len(i) == 4 or len(i) == 3 or len(i) == 7:
                 retval += 1
 
